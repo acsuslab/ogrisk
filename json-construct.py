56d77fbd86d1828a27a4f28acd59f2d1a095bf7e
lim -cm <mynameispi3.14159@gmail.com>
2024-03-04 13:00:54 -0500

    make changes to json-construct

--- json-construct.py
+++ json-construct.py
@@ -12,7 +12,17 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
 data = {}
-data["open_source_contracts"] = []
+data['current_contract_index'] = 0
+data['open_source_contracts'] = []
+
+#Specify the file path to write the data to
+file_path = "data-test.json"
+
+if os.path.exists(file_path):
+    with open(file_path, 'r') as json_file:
+        existing_data = json.load(json_file)
+    data['current_contract_index'] = existing_data['current_contract_index']
+    data['open_source_contracts'] = existing_data['open_source_contracts']
 
 def getTxnData(txnHash):
 
@@ -49,16 +59,17 @@ def getCompilerVersion(address):
 
 def getAddressData(address):
     result = []
-    page = 1
     num_oog_txn = 0
+    total_num_transactions = 0
+    startblock = 0
 
     params = {
         'module': 'account',
         'action': 'txlist',
         'address': address,
-        'startblock': 0,
+        'startblock': startblock,
         'endblock': 99999999,
-        'page': page,
+        'page': 1,
         'offset': 0,
         'sort': 'asc',
         'apikey': API_KEY
@@ -75,18 +86,40 @@ def getAddressData(address):
         result.append(data['status'])
         #get the logs from the api call
         logs = data['result']
-        #Append the total number of transactions to the result
-        result.append(len(logs))
-        if(len(logs) > 0):
+
+        while(True):
+            #getting the total number of transactions
+            total_num_transactions += len(logs)
             #looping through the transactions to get how many transactions are out of gas
-            num_oog_txn = 0
             for txn in logs:
                 if(txn['isError'] == "1" and getTxnData(txn['hash']).lower() == "out of gas"):
                     num_oog_txn += 1
-            #appending the number of out of gas transactions to the result
-            result.append(num_oog_txn)
-            #appending the compiler version to the result
-            result.append(getCompilerVersion(address))
+            #checking if logs are less than 10000, if they are then break out of loop, if not then get more txns
+            if(len(logs) < 10000):
+                break
+            else:
+                #setting the new start block for next iteration
+                startblock = logs[len(logs) - 1]['blockNumber']
+                params['startblock'] = startblock
+                #looping from last transaction to first transaction
+                #to remove all transactions that are in the last block
+                i = 0
+                while(logs[(len(logs) - 1) - i]['blockNumber'] == startblock):
+                    if(logs[(len(logs) - 1) - i]['blockNumber'] == startblock):
+                        total_num_transactions -= 1
+                        if(logs[(len(logs) - 1) - i]['isError'] == "1" and 
+                            getTxnData(logs[(len(logs) - 1) - i]['hash']).lower() == "out of gas"):
+                            num_oog_txn -= 1
+                    i += 1
+                response = requests.get(ETHERSCAN_API_URL, params=params)
+                data = response.json()
+                logs = data['result']
+        #Append the total number of transactions to the result
+        result.append(total_num_transactions)
+        #appending the number of out of gas transactions to the result
+        result.append(num_oog_txn)
+        #appending the compiler version to the result
+        result.append(getCompilerVersion(address))
     else:
         print(f"API request error: {data['message']}\n")
         #append the status number as the beginning of the list
@@ -94,7 +127,7 @@ def getAddressData(address):
     return result
 
 #!!!only getting from test folder
-with os.scandir("/Volumes/External-Storage/smart-contract-sanctuary-ethereum/contracts/test3") as it:
+with os.scandir("/Volumes/External-Storage/smart-contract-sanctuary-ethereum/contracts/test") as it:
     for entry in it:
         if entry.name.endswith(".sol") and entry.is_file():
             split_filename = entry.name.split("_")
@@ -105,17 +138,15 @@ with os.scandir("/Volumes/External-Storage/smart-contract-sanctuary-ethereum/con
             #if(addressData[0] == 0 or addressData[2] == 0):
                 #data["open_source_contracts"].append({"address": address})
             if(addressData[0] != 0 and addressData[2] != 0):
-                data["open_source_contracts"].append({"address": address,
+                data['open_source_contracts'].append({"address": address,
                                                     "total_number_of_txns": addressData[1],
                                                     "total_number_of_oug_txns": addressData[2],
                                                     "compiler version": addressData[3]})
-
-
-#Writing to the JSON file
-            
-# Specify the file path
-file_path = "data-test.json"
-
-# Write data to the JSON file
-with open(file_path, 'w') as json_file:
-    json.dump(data, json_file, indent=2)
+                data['current_contract_index'] += 1
+            else:
+                data['open_source_contracts'].append({"address": address,
+                                                    "total_number_of_txns": -1})
+                data['current_contract_index'] += 1
+        # Write data to the JSON file
+        with open(file_path, 'w') as json_file:
+            json.dump(data, json_file, indent=2)
